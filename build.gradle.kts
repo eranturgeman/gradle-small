5b523252ba70e17e72bb1c7311d1a5401066cd94
Eran Turgeman <erant@jfrog.com>
2023-09-13 15:42:00 +0300

    add kotlin build file

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+val spi: Configuration by configurations.creating
+
+dependencies {
+    implementation(project(":project"))
+
+    runtimeOnly('junit:junit:4.7')
+    runtimeOnly("junit:junit:4.7")
+    implementation('junit:junit:4.7:javadoc')
+    runtimeOnly("commons-io:commons-io:1.2") {
+        isTransitive = true
+    }
+
+    runtimeOnly(group = 'junit', name = 'junit', version = '4.7')
+    runtimeOnly(group = "junit", name = "junit", version = "4.7")
+    runtimeOnly(group = 'junit', name = 'junit', version = '4.7', classifier = 'javadoc')
+    runtimeOnly(group = "commons-io", name = "commons-io", version = "1.2") {
+        isTransitive = true
+    }
+
+    testCompile("junit", "junit", "4.7")
+}
+
+// Just a smoke test that using this option does not lead to any exception
+tasks {
+    named<JavaCompile>("compileJava") {
+        options.compilerArgs = listOf("-Xlint:unchecked")
+    }
+}
